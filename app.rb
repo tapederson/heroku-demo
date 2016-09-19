76d382330f6ae090ea813228ce44d7d461f8c5b0
Tony Pederson <tonypederson@Tonys-MacBook-Pro-2.local>
2016-09-19 17:53:01 -0500

    add contact list

--- app.rb
+++ app.rb
@@ -4,6 +4,15 @@ require 'sinatra'
 require 'sinatra/activerecord'
 require './environments'
 
+class Contact < ActiveRecord::Base
+  self.table_name = 'salesforce.contact'
+end
+
+get "/contacts" do
+  @contacts = Contact.all
+  erb :index
+End
+
 #calling db
 class Contact < ActiveRecord::Base
   self.table_name = 'salesforce.contact'
